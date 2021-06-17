9a9dee1443a2ac09e5f6aa30eb7923678a7f3850
Sebastian Socol <sebastian.socol@outlook.com>
2021-06-17 12:22:55 +0100

    Migration creation fixed, added migrations

---
+++ WebScrapper.API/WebScrapper.Data/Migrations/20210617111951_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebScrapper.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SearchResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Keywords = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ranking = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SearchDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Occurrences = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SearchResults", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SearchResults");
+        }
+    }
+}
